bd52dbe069adc5cdc0f3750fc71c55e4729a3071
dipodidae <senseosoft@gmail.com>
2016-10-09 17:41:33 +0200

    design

--- dist/js/data/questions.js
+++ dist/js/data/questions.js
@@ -12,42 +12,6 @@ define(function() {
 			],
 			"correct": "c",
 			"feedback": "Varkens hebben modderbaden nodig. Zij hebben daar verschillende redenen voor. Als wij het te warm krijgen, beginnen we te zweten waardoor ons lichaam afkoelt. Varkens kunnen niet zweten, daarom rollen ze door de modder en zoeken zo verkoeling. Bovendien beschermt die modderlaag hen tegen vliegen, muggen en parasieten. Hun huid moeten ze regelmatig reinigen. Niet door een bad of douche te nemen zoals jij, maar door te schuren tegen bomen of muren. Tevens poetsen ze elkaar op plaatsen waar ze zelf niet goed bij kunnen"
-		},
-
-		{
-			"question": "Wat doet dit varken? 2",
-			"image": "mudbath.jpg",
-			"answers": [
-				{"key": "a", "answer": "niks"},
-				{"key": "b", "answer": "heel veel"},
-				{"key": "c", "answer": "een heleboel"},
-			],
-			"correct": "a",
-			"feedback": "Omdat daarom!"
-		},
-
-		{
-			"question": "Wat doet dit varken? 3",
-			"image": "mudbath.jpg",
-			"answers": [
-				{"key": "a", "answer": "niks"},
-				{"key": "b", "answer": "heel veel"},
-				{"key": "c", "answer": "een heleboel"},
-			],
-			"correct": "a",
-			"feedback": "Omdat daarom!"
-		},
-		
-		{
-			"question": "Wat doet dit varken? 4",
-			"image": "mudbath.jpg",
-			"answers": [
-				{"key": "a", "answer": "niks"},
-				{"key": "b", "answer": "heel veel"},
-				{"key": "c", "answer": "een heleboel"},
-			],
-			"correct": "a",
-			"feedback": "Omdat daarom!"
 		}
 	];
 });
