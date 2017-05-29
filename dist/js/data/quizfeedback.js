def084b1a45bb6553bcc1430e99433e380994b9a
dipodidae <senseosoft@gmail.com>
2017-05-29 22:32:09 +0200

    feedback update

--- dist/js/data/quizfeedback.js
+++ dist/js/data/quizfeedback.js
@@ -3,27 +3,22 @@ define(function() {
 	return [
 		{
 			"withinPercentage": 0,
-			"feedback": "Wow, geen enkel antwoord goed. Gelukkig hebben anarchisten met elkaar afgesproken dat ze niet al te merkbaar kameraden uitlachen."
-		},
-
-		{
-			"withinPercentage": 20,
-			"feedback": "Wauw, je hebt een beetje goed. "
+			"feedback": "Wow, (bijna) geen enkel antwoord goed. Gelukkig hebben anarchisten met elkaar afgesproken dat ze niet al te merkbaar kameraden uitlachen. Misschien kun je je kennis opdoen bij één van de workshops, bijvoorbeeld: de lezing door Anarchistische groep Skya uit Griekenland, op zondag om 11:30 in de grote zaal. "
 		},
 
 		{
 			"withinPercentage": 50,
-			"feedback": "Je weet een beetje over anarchisme"
+			"feedback": "Zo te zie ben je al redelijk ingewijd in anarchistische theorie. Wil je meer weten over anti-authoritaire strijd anno nu? Check bijvoorbeeld de ‘University of Color: lecture and discussion.’ (zondag, 10:15, gymzaal)"
 		},
 
 		{
 			"withinPercentage": 70,
-			"feedback": "Je weet veel over anarchisme. "
+			"feedback": "Wow, goeie score! Waarschijnlijk kan alleen de historische lezig over de Moker (zondag 9:00, grote zaal) jou nog prikkelen. "
 		},
 		
 		{
 			"withinPercentage": 100,
-			"feedback": "Je bent een geweldige anarchist!"
+			"feedback": "Wow, perfecte score! Waarschijnlijk kan alleen de historische lezig over de Moker (zondag 9:00, grote zaal) jou nog prikkelen."
 		}
 	];
 });
